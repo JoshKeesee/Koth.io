64e509a28876e85580c1a598ba789e9b38c64426
Joshua <105324950+JoshKeesee@users.noreply.github.com>
2022-12-23 17:45:08 +0000

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = new Server(httpServer, {
     origin: ["https://admin.socket.io"],
     credentials: true
   },
-  maxHttpBufferSize: 5e6
+  maxHttpBufferSize: 1e7
 });
 
 instrument(io, {
@@ -38,14 +38,14 @@ io.on("connection", (socket) => {
   gameState.players[socket.id] = {
     name: null
   };
-  
+
   socket.on("disconnect", () => {
     for (var i = 0; i < Object.keys(gameState.players).length; i++) {
       if (gameState.players[Object.keys(gameState.players)[i]].lt === socket.id) {
         gameState.players[Object.keys(gameState.players)[i]].lt = null;
       }
     }
-    
+
     delete gameState.players[socket.id];
     delete gameState.leaderboard[socket.id];
   });
@@ -58,7 +58,7 @@ io.on("connection", (socket) => {
     }
 
     playerNum = Object.keys(gameState.leaderboard).length + 1
-    
+
     gameState.players[socket.id] = {
       x: playerNum * 100,
       y: 320,
@@ -81,10 +81,10 @@ io.on("connection", (socket) => {
 
     gameState.leaderboard[socket.id] = 0;
   });
-  
+
   socket.on("playerMovement", (data) => {
     var playerMovement = data[0];
-    
+
     defaults = {
       gravity: 0.4,
       resistance: 0.9,
@@ -92,17 +92,17 @@ io.on("connection", (socket) => {
       maxJumps: data[1].jumps,
       jumpForce: 15
     };
-    
+
     const player = gameState.players[socket.id];
     player.speed = data[2];
     player.pushForce = data[1].force;
     player.knockback = data[1].padding;
     player.touching = false;
-    
+
     if (playerMovement.l) {
       player.xVel -= defaults.speed;
     }
-    
+
     if (playerMovement.r) {
       player.xVel += defaults.speed;
     }
@@ -133,101 +133,103 @@ io.on("connection", (socket) => {
       if (player.lt !== null) {
         gameState.leaderboard[player.lt] += gameState.players[player.lt].pps;
         gameState.leaderboard = Object.entries(gameState.leaderboard)
-        .sort(([,a],[,b]) => a - b)
-        .reverse()
-        .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+          .sort(([, a], [, b]) => a - b)
+          .reverse()
+          .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
         player.lt = null;
       }
     }
 
-    for (let i = 0; i < playerMovement.platforms.length; i++) {
-      var top = playerMovement.platforms[i].t;
-      var bottom = playerMovement.platforms[i].b;
-      var left = playerMovement.platforms[i].l;
-      var right = playerMovement.platforms[i].r;
-      var width = playerMovement.platforms[i].w;
-      var height = playerMovement.platforms[i].h;
-      var type = playerMovement.platforms[i].type;
-      var playerId = playerMovement.platforms[i].playerId;
+    collision(player, playerMovement.platforms);
 
-      if (player.x + player.w > left) {
-        if (player.x + player.w < left + (width / 2) && player.y < bottom - (height / 2) && player.y > top - player.h + (height / 2)) {
-          if (type === "player") {
-            player.touching = true;
-            if (Math.abs(player.xVel) > Math.abs(gameState.players[playerId].xVel)) {
-              gameState.players[playerId].xVel = (player.xVel * player.pushForce) * gameState.players[playerId].knockback;
-              player.xVel = 0 - (player.xVel * defaults.resistance);
-              player.yVel = 0;
-            } else {
-              player.xVel = gameState.players[playerId].xVel * gameState.players[playerId].pushForce;
-            }
-            gameState.players[playerId].lt = player.id;
-          } else {
-            player.xVel = 0;
-            player.yVel = 0;
-          }
-          player.x = left - player.w;
-          player.jumps = 0;
-        }
-      }
+    // for (let i = 0; i < playerMovement.platforms.length; i++) {
+    //   var top = playerMovement.platforms[i].t;
+    //   var bottom = playerMovement.platforms[i].b;
+    //   var left = playerMovement.platforms[i].l;
+    //   var right = playerMovement.platforms[i].r;
+    //   var width = playerMovement.platforms[i].w;
+    //   var height = playerMovement.platforms[i].h;
+    //   var type = playerMovement.platforms[i].type;
+    //   var playerId = playerMovement.platforms[i].playerId;
 
-      if (player.x < right) {
-        if (player.x > right - (width / 2) && player.y < bottom - (height / 2) && player.y > top - player.h + (height / 2)) {
-          if (type === "player") {
-            player.touching = true;
-            if (Math.abs(player.xVel) > Math.abs(gameState.players[playerId].xVel)) {
-              gameState.players[playerId].xVel = (player.xVel * player.pushForce) * gameState.players[playerId].knockback;
-              player.xVel = 0 - (player.xVel * defaults.resistance);
-              player.yVel = 0;
-            } else {
-              player.xVel = gameState.players[playerId].xVel * gameState.players[playerId].pushForce;
-            }
-            gameState.players[playerId].lt = player.id;
-          } else {
-            player.xVel = 0;
-            player.yVel = 0;
-          }
-          player.x = right;
-          player.jumps = 0;
-        }
-      }
+    //   if (player.x + player.w > left) {
+    //     if (player.x + player.w < left + (width / 2) && player.y < bottom - (height / 2) && player.y > top - player.h + (height / 2)) {
+    //       if (type === "player") {
+    //         player.touching = true;
+    //         if (Math.abs(player.xVel) > Math.abs(gameState.players[playerId].xVel)) {
+    //           gameState.players[playerId].xVel = (player.xVel * player.pushForce) * gameState.players[playerId].knockback;
+    //           player.xVel = 0 - (player.xVel * defaults.resistance);
+    //           player.yVel = 0;
+    //         } else {
+    //           player.xVel = gameState.players[playerId].xVel * gameState.players[playerId].pushForce;
+    //         }
+    //         gameState.players[playerId].lt = player.id;
+    //       } else {
+    //         player.xVel = 0;
+    //         player.yVel = 0;
+    //       }
+    //       player.x = left - player.w;
+    //       player.jumps = 0;
+    //     }
+    //   }
 
-      if (player.y > top - player.h - 2) {
-        if (player.x < right && player.y < top - player.h + (height / 2) - 2 && player.x + player.w > left) {
-          if (type === "player") {
-            player.touching = true;
-            player.yVel = 0 - (player.yVel * 0.8);
-            gameState.players[playerId].lt = player.id;
-            if (playerMovement.u) {
-              player.yVel -= defaults.jumpForce;
-            }
-          } else {
-            player.yVel = 0;
-          }
-          player.y = top - player.h - 2;
-          player.jumps = 0;
-        }
-      }
+    //   if (player.x < right) {
+    //     if (player.x > right - (width / 2) && player.y < bottom - (height / 2) && player.y > top - player.h + (height / 2)) {
+    //       if (type === "player") {
+    //         player.touching = true;
+    //         if (Math.abs(player.xVel) > Math.abs(gameState.players[playerId].xVel)) {
+    //           gameState.players[playerId].xVel = (player.xVel * player.pushForce) * gameState.players[playerId].knockback;
+    //           player.xVel = 0 - (player.xVel * defaults.resistance);
+    //           player.yVel = 0;
+    //         } else {
+    //           player.xVel = gameState.players[playerId].xVel * gameState.players[playerId].pushForce;
+    //         }
+    //         gameState.players[playerId].lt = player.id;
+    //       } else {
+    //         player.xVel = 0;
+    //         player.yVel = 0;
+    //       }
+    //       player.x = right;
+    //       player.jumps = 0;
+    //     }
+    //   }
 
-      if (player.y < bottom) {
-        if (player.x < right && player.y > bottom - (height / 2) && player.x + player.w > left) {
-          if (type === "player") {
-            player.touching = true;
-            gameState.players[playerId].yVel = (player.yVel * player.pushForce) * gameState.players[playerId].knockback;
-            gameState.players[playerId].lt = player.id;
-            player.yVel = 1;
-          } else {
-            player.yVel = 1;
-          }
-          player.y = bottom;
-        }
-      }
-    }
+    //   if (player.y > top - player.h - 2) {
+    //     if (player.x < right && player.y < top - player.h + (height / 2) - 2 && player.x + player.w > left) {
+    //       if (type === "player") {
+    //         player.touching = true;
+    //         player.yVel = 0 - (player.yVel * 0.8);
+    //         gameState.players[playerId].lt = player.id;
+    //         if (playerMovement.u) {
+    //           player.yVel -= defaults.jumpForce;
+    //         }
+    //       } else {
+    //         player.yVel = 0;
+    //       }
+    //       player.y = top - player.h - 2;
+    //       player.jumps = 0;
+    //     }
+    //   }
+
+    //   if (player.y < bottom) {
+    //     if (player.x < right && player.y > bottom - (height / 2) && player.x + player.w > left) {
+    //       if (type === "player") {
+    //         player.touching = true;
+    //         gameState.players[playerId].yVel = (player.yVel * player.pushForce) * gameState.players[playerId].knockback;
+    //         gameState.players[playerId].lt = player.id;
+    //         player.yVel = 1;
+    //       } else {
+    //         player.yVel = 1;
+    //       }
+    //       player.y = bottom;
+    //     }
+    //   }
+    // }
 
     if (!playerMovement.u || player.yVel == 0) {
       player.jumping = false;
     }
-    
+
     if (playerMovement.u && player.jumps < defaults.maxJumps && !player.jumping && !playerMovement.flying) {
       player.jumping = true;
       player.jumps++;
@@ -249,7 +251,7 @@ io.on("connection", (socket) => {
     if (playerMovement.flying) {
       player.yVel *= defaults.resistance;
     }
-    
+
     player.xVel *= defaults.resistance;
     player.y += player.yVel;
     player.x += player.xVel;
@@ -281,6 +283,58 @@ io.on("connection", (socket) => {
     socket.emit("state", gameState);
   }, 1000 / 60);
 });
+
+function colliding(r1, r2) {
+  return !(r1.x > r2.l + r2.w || r1.x + r1.w < r2.l || r1.y > r2.t + r2.h || r1.y + r1.h < r2.t);
+}
+
+function colR(r1, r2) {
+  return (r1.x < r2.l + r2.w && r1.x > r2.l + r2.w - Math.abs(r1.xVel));
+}
+
+function colL(r1, r2) {
+  return (r1.x + r1.w > r2.l && r1.x + r1.w < r2.l + Math.abs(r1.xVel));
+}
+
+function colB(r1, r2) {
+  return (r1.y < r2.t + r2.h && r1.y > r2.t - Math.abs(r1.yVel));
+}
+
+function colT(r1, r2) {
+  if (r1.yVel <= 1) {
+    return (r1.y + r1.h > r2.t && r1.y + r1.h < r2.t + 5);
+  } else {
+    return (r1.y + r1.h > r2.t && r1.y + r1.h < r2.t + Math.abs(r1.yVel));
+  }
+}
+
+function collision(player, platforms) {
+  for (let i = 0; i < platforms.length; i++) {
+    if (colliding(player, platforms[i])) {
+      if (colB(player, platforms[i])) {
+        player.y += Math.abs(player.yVel) + 1;
+        player.yVel = 1;
+      }
+
+      if (colT(player, platforms[i])) {
+        // player.y -= Math.abs(player.yVel);
+        player.y = platforms[i].t - player.h;
+        player.yVel = 0;
+        player.jumps = 0;
+      }
+
+      if (colL(player, platforms[i])) {
+        player.x -= Math.abs(player.xVel) + 1;
+        player.xVel = 0;
+      }
+
+      if (colR(player, platforms[i])) {
+        player.x += Math.abs(player.xVel) + 1;
+        player.xVel = 0;
+      }
+    }
+  }
+}
 
 httpServer.listen(port, () => {
   console.log(`Server listening on port ${port}`)
